b36daf743e0eebd43401906d9ef721198b89326f
tearfu1 <axuior12@gmail.com>
2023-05-22 10:59:56 +0200

    text box fix

--- textbox/inc/textbox.h
+++ textbox/inc/textbox.h
@@ -35,26 +35,25 @@ namespace tb
     public:
         TextBox()
         {
-            m_newText = "Input func";
-            std::cout << *m_newText.getData();
-            m_newText.insert(1, 'k');
+            m_newText = "Input func dude please Input func dude please";
             font.loadFromFile("my_font.ttf");
             
             v_carriage = 0;
             carriage = -1;
             l_bound = 0;
-            r_bound = 4;
+            r_bound = 24;
             
-            m_text.setPosition(400, 500);
-            m_text.setString(m_newText.substring(l_bound,5));
+            m_text.setPosition(100, 500);
+            m_text.setString(m_newText.substring(l_bound,25));
             m_text.setFont(font);
             
-            carriage_box.setSize(sf::Vector2f(1, 30));
+            carriage_box.setSize(sf::Vector2f(1, 35));
             updateVCarriage();
 
             m_box.setFillColor(sf::Color::Blue);
-            m_box.setSize(sf::Vector2f(200, 40));
-            m_box.setPosition(400, 500);
+            m_box.setSize(sf::Vector2f(370, 40));
+            m_box.setPosition(100, 500);
+            
             
         }
         virtual void draw(sf::RenderTarget& render, sf::RenderStates states) const
@@ -75,13 +74,10 @@ namespace tb
         {
             if (event.type == sf::Event::TextEntered)
             {
-                std::cout << m_newText.getSize()<<std::endl;
-                std::cout << carriage<<std::endl;
                 //Обработка ввода
                 switch (event.text.unicode)
                 {
                 case 0xD: //Return
-                    std::cout << "q";
                     if (carriage != -1)
                     {
                         char temp_char = event.text.unicode;
@@ -89,7 +85,7 @@ namespace tb
                         m_newText.insert(carriage + 1, temp_char);
                         carriage++;
                         updateText();
-                        if (v_carriage != 4)
+                        if (v_carriage != 25)
                         {
                             v_carriage++;
                             updateVCarriage();
@@ -106,18 +102,31 @@ namespace tb
                     }
                     break;
                 case 0x8://Backspace
+                    std::cout << carriage << " " << v_carriage << std::endl;
                     if (!m_newText.isEmpty())
                     {
                         if (carriage != -1)
                         {
                             m_newText.erase(carriage);
+                            if (v_carriage == 0)
+                            {
+                                updateText();
+                                if (r_bound - l_bound < 24)
+                                {
+                                    v_carriage = carriage;
+                                    updateVCarriage();
+                                }
+                                
+                            }
+                            else
+                            {
+                                updateText();
+                                v_carriage--;
+                                updateVCarriage();
+                                
+                            }
                             updateText();
                             carriage--;
-                            if (v_carriage != 0)
-                            {
-                                v_carriage--;
-                            }
-                            updateVCarriage();
                         }
                     }
                     
@@ -129,7 +138,7 @@ namespace tb
                         m_newText.insert(carriage+1, static_cast<wchar_t>(event.text.unicode));
                         carriage++;
                         updateText();
-                        if (v_carriage != 4)
+                        if (v_carriage != 25)
                         {
                             v_carriage++;
                             updateVCarriage();
@@ -160,24 +169,33 @@ namespace tb
                                 v_carriage--;
                                 updateVCarriage();
                             }
-                            if (v_carriage == 0) updateText();
+                            if (v_carriage == 0)
+                            {
+                                updateText();
+                            }
                             carriage--;
-                            
                         }
                     }
                     break;
                 case sf::Keyboard::Right:
+                    
                     if (!m_newText.isEmpty())
                     {
                         if (carriage != (m_newText.getSize() - 1))
-                            carriage++;
-                        if (v_carriage != 4 && v_carriage!=m_newText.getSize())
                         {
-                            v_carriage++;
-                            updateVCarriage();
+                            carriage++;
+                            if (v_carriage != 25)
+                            {
+                                v_carriage++;
+                                updateVCarriage();
+                            }
+                            else if (v_carriage == 25)
+                            {
+                                updateVCarriage();
+                                updateText();
+                            }
                         }
                     }
-                    if(v_carriage==4)updateText();
                     break;
                 default:
                     break;
@@ -189,6 +207,14 @@ namespace tb
         {
             return m_box.getGlobalBounds().contains(x, y);
         }
+        auto getStr()
+        {
+            return m_newText.getData();
+        }
+        int getSize()
+        {
+            return m_newText.getSize();
+        }
         private:
         void setText(const sf::String& str)
         {
@@ -199,11 +225,11 @@ namespace tb
             sf::String temp;
             if (carriage > r_bound)
             {
-                if (m_newText.getSize() > 5)
+                if (m_newText.getSize() > 25)
                 {
-                    temp = m_newText.substring(carriage - 4, 5);
+                    temp = m_newText.substring(carriage - 24, 25);
                     r_bound = carriage;
-                    l_bound = carriage - 4;
+                    l_bound = carriage - 24;
                 }
                 else
                 {
@@ -213,10 +239,10 @@ namespace tb
             }
             else if (carriage < l_bound)
             {
-                if (m_newText.getSize() > 5)
+                if (m_newText.getSize() > 25)
                 {
-                    temp = m_newText.substring(carriage, 5);
-                    r_bound = carriage + 4;
+                    temp = m_newText.substring(carriage, 25);
+                    r_bound = carriage + 24;
                     l_bound = carriage;
                 }
                 else
@@ -229,14 +255,16 @@ namespace tb
             {
                 if (!m_newText.isEmpty())
                 {
-                    temp = m_newText.substring(l_bound, 5);
+                    temp = m_newText.substring(l_bound, 25);
                 }
             }
             setText(temp);
         }
         void updateVCarriage()
         {
-            carriage_box.setPosition(400 + v_carriage * 16 + 2, 505);
+            //std::cout << carriage << " " << v_carriage<<std::endl;
+                
+            carriage_box.setPosition((m_text.findCharacterPos(v_carriage)).x +1, 505);
         }
     };
 
