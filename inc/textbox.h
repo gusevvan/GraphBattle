6c31e08d5f022f5c062a88bfe221568bb7e87fe4
tearfu1 <axuior12@gmail.com>
2023-05-22 13:29:45 +0200

    textbox devided

--- inc/textbox.h
+++ inc/textbox.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <SFML/Graphics.hpp>
-#include <iostream>
 
 
 namespace tb
@@ -33,239 +32,18 @@ namespace tb
 
         int v_carriage;
     public:
-        TextBox()
-        {
-            m_newText = "";
-            font.loadFromFile("my_font.ttf");
-            
-            v_carriage = 0;
-            carriage = -1;
-            l_bound = 0;
-            r_bound = 24;
-            
-            m_text.setPosition(100, 500);
-            m_text.setString(m_newText.substring(l_bound,25));
-            m_text.setFont(font);
-            
-            carriage_box.setSize(sf::Vector2f(1, 35));
-            updateVCarriage();
-
-            m_box.setFillColor(sf::Color::Blue);
-            m_box.setSize(sf::Vector2f(370, 40));
-            m_box.setPosition(100, 500);
-            
-            
-        }
-        virtual void draw(sf::RenderTarget& render, sf::RenderStates states) const
-        {
-            render.draw(m_box, states);
-            render.draw(m_text, states);
-            render.draw(carriage_box, states);
-        }
-        void setFocus() override
-        {
-            m_box.setFillColor(sf::Color::Red);
-        }
-        void deleteFocus() override
-        {
-            m_box.setFillColor(sf::Color::Blue);
-        }
-        virtual void event(const sf::Event& event)
-        {
-            if (event.type == sf::Event::TextEntered)
-            {
-                //Обработка ввода
-                switch (event.text.unicode)
-                {
-                case 0xD: //Return
-                    if (carriage != -1)
-                    {
-                        char temp_char = event.text.unicode;
-                        std::cout << temp_char << std::endl;
-                        m_newText.insert(carriage + 1, temp_char);
-                        carriage++;
-                        updateText();
-                        if (v_carriage != 25)
-                        {
-                            v_carriage++;
-                            updateVCarriage();
-                        }
-                    }
-                    else
-                    {
-                        char temp_char = event.text.unicode;
-                        m_newText.insert(0, temp_char);
-                        carriage++;
-                        v_carriage++;
-                        updateVCarriage();
-                        updateText();
-                    }
-                    break;
-                case 0x8://Backspace
-                    std::cout << carriage << " " << v_carriage << std::endl;
-                    if (!m_newText.isEmpty())
-                    {
-                        if (carriage != -1)
-                        {
-                            m_newText.erase(carriage);
-                            if (v_carriage == 0)
-                            {
-                                updateText();
-                                if (r_bound - l_bound < 24)
-                                {
-                                    v_carriage = carriage;
-                                    updateVCarriage();
-                                }
-                                
-                            }
-                            else
-                            {
-                                updateText();
-                                v_carriage--;
-                                updateVCarriage();
-                                
-                            }
-                            updateText();
-                            carriage--;
-                        }
-                    }
-                    
-                    break;
-                default:
-                {
-                    if (carriage != -1)
-                    {
-                        m_newText.insert(carriage+1, static_cast<wchar_t>(event.text.unicode));
-                        carriage++;
-                        updateText();
-                        if (v_carriage != 25)
-                        {
-                            v_carriage++;
-                            updateVCarriage();
-                        }
-                    }
-                    else
-                    {
-                        m_newText.insert(0, static_cast<wchar_t>(event.text.unicode));
-                        carriage++;
-                        v_carriage++;
-                        updateVCarriage();
-                        updateText();
-                    }
-                }
-                }
-            }
-            else if (event.type == sf::Event::KeyPressed)
-            {
-                switch (event.key.code)
-                {
-                case sf::Keyboard::Left:
-                    if (!m_newText.isEmpty())
-                    {
-                        if (carriage != -1)
-                        {
-                            if (v_carriage != 0)
-                            {
-                                v_carriage--;
-                                updateVCarriage();
-                            }
-                            if (v_carriage == 0)
-                            {
-                                updateText();
-                            }
-                            carriage--;
-                        }
-                    }
-                    break;
-                case sf::Keyboard::Right:
-                    
-                    if (!m_newText.isEmpty())
-                    {
-                        if (carriage != (m_newText.getSize() - 1))
-                        {
-                            carriage++;
-                            if (v_carriage != 25)
-                            {
-                                v_carriage++;
-                                updateVCarriage();
-                            }
-                            else if (v_carriage == 25)
-                            {
-                                updateVCarriage();
-                                updateText();
-                            }
-                        }
-                    }
-                    break;
-                default:
-                    break;
-                }
-            }
-        }
-        
-        bool Contains(const int& x, const int& y)
-        {
-            return m_box.getGlobalBounds().contains(x, y);
-        }
-        auto getStr()
-        {
-            return m_newText.getData();
-        }
-        int getSize()
-        {
-            return m_newText.getSize();
-        }
+        TextBox();
+        virtual void draw(sf::RenderTarget& render, sf::RenderStates states) const;
+        void setFocus() override;
+        void deleteFocus() override;
+        virtual void event(const sf::Event& event);
+        bool Contains(const int& x, const int& y);
+        auto getStr();
+        int getSize();
         private:
-        void setText(const sf::String& str)
-        {
-            m_text.setString(str);
-        }
-        void updateText()
-        {
-            sf::String temp;
-            if (carriage > r_bound)
-            {
-                if (m_newText.getSize() > 25)
-                {
-                    temp = m_newText.substring(carriage - 24, 25);
-                    r_bound = carriage;
-                    l_bound = carriage - 24;
-                }
-                else
-                {
-                    r_bound = carriage;
-                    l_bound = 0;
-                }
-            }
-            else if (carriage < l_bound)
-            {
-                if (m_newText.getSize() > 25)
-                {
-                    temp = m_newText.substring(carriage, 25);
-                    r_bound = carriage + 24;
-                    l_bound = carriage;
-                }
-                else
-                {
-                    r_bound = carriage;
-                    l_bound = 0;
-                }
-            }
-            else
-            {
-                if (!m_newText.isEmpty())
-                {
-                    temp = m_newText.substring(l_bound, 25);
-                }
-            }
-            setText(temp);
-        }
-        void updateVCarriage()
-        {
-            //std::cout << carriage << " " << v_carriage<<std::endl;
-                
-            carriage_box.setPosition((m_text.findCharacterPos(v_carriage)).x +1, 505);
-        }
+        void setText(const sf::String& str);
+        void updateText();
+        void updateVCarriage();
     };
 
 
@@ -276,32 +54,10 @@ namespace tb
         FocusObject* m_object;
     public:
 
-        FocusController(FocusObject* obj = 0) : m_object(obj)
-        {
-            if (m_object != 0)
-            {
-                m_object->setFocus();
-            }
-        }
-        void setFocusObject(FocusObject* new_obj)
-        {
-            if (m_object == new_obj)
-                return;
-            if (m_object != 0)
-                m_object->deleteFocus();
-            if (new_obj != 0)
-                new_obj->setFocus();
-            m_object = new_obj;
-        }
-        FocusObject* getFocusObject()
-        {
-            return m_object;
-        }
-
+        FocusController(FocusObject* obj = 0);
+        void setFocusObject(FocusObject* new_obj);
+        FocusObject* getFocusObject();
     
     };
 
 }
-
-
-
---
+++ src/textbox.cpp
@@ -0,0 +1,252 @@
+#include "textbox.h"
+using namespace tb;
+TextBox::TextBox()
+{
+    m_newText = "";
+    font.loadFromFile("my_font.ttf");
+
+    v_carriage = 0;
+    carriage = -1;
+    l_bound = 0;
+    r_bound = 24;
+
+    m_text.setPosition(100, 500);
+    m_text.setString(m_newText.substring(l_bound, 25));
+    m_text.setFont(font);
+
+    carriage_box.setSize(sf::Vector2f(1, 35));
+    updateVCarriage();
+
+    m_box.setFillColor(sf::Color::Blue);
+    m_box.setSize(sf::Vector2f(370, 40));
+    m_box.setPosition(100, 500);
+
+}
+void TextBox::draw(sf::RenderTarget& render, sf::RenderStates states) const
+{
+    render.draw(m_box, states);
+    render.draw(m_text, states);
+    render.draw(carriage_box, states);
+}
+void TextBox::setFocus()
+{
+    m_box.setFillColor(sf::Color::Red);
+}
+void TextBox::deleteFocus() 
+{
+    m_box.setFillColor(sf::Color::Blue);
+}
+void TextBox::event(const sf::Event& event)
+{
+    if (event.type == sf::Event::TextEntered)
+    {
+        //Обработка ввода
+        switch (event.text.unicode)
+        {
+        case 0xD: //Return
+            if (carriage != -1)
+            {
+                char temp_char = event.text.unicode;
+                m_newText.insert(carriage + 1, temp_char);
+                carriage++;
+                updateText();
+                if (v_carriage != 25)
+                {
+                    v_carriage++;
+                    updateVCarriage();
+                }
+            }
+            else
+            {
+                char temp_char = event.text.unicode;
+                m_newText.insert(0, temp_char);
+                carriage++;
+                v_carriage++;
+                updateVCarriage();
+                updateText();
+            }
+            break;
+        case 0x8://Backspace
+            if (!m_newText.isEmpty())
+            {
+                if (carriage != -1)
+                {
+                    m_newText.erase(carriage);
+                    if (v_carriage == 0)
+                    {
+                        updateText();
+                        if (r_bound - l_bound < 24)
+                        {
+                            v_carriage = carriage;
+                            updateVCarriage();
+                        }
+
+                    }
+                    else
+                    {
+                        updateText();
+                        v_carriage--;
+                        updateVCarriage();
+
+                    }
+                    updateText();
+                    carriage--;
+                }
+            }
+
+            break;
+        default:
+        {
+            if (carriage != -1)
+            {
+                m_newText.insert(carriage + 1, static_cast<wchar_t>(event.text.unicode));
+                carriage++;
+                updateText();
+                if (v_carriage != 25)
+                {
+                    v_carriage++;
+                    updateVCarriage();
+                }
+            }
+            else
+            {
+                m_newText.insert(0, static_cast<wchar_t>(event.text.unicode));
+                carriage++;
+                v_carriage++;
+                updateVCarriage();
+                updateText();
+            }
+        }
+        }
+    }
+    else if (event.type == sf::Event::KeyPressed)
+    {
+        switch (event.key.code)
+        {
+        case sf::Keyboard::Left:
+            if (!m_newText.isEmpty())
+            {
+                if (carriage != -1)
+                {
+                    if (v_carriage != 0)
+                    {
+                        v_carriage--;
+                        updateVCarriage();
+                    }
+                    if (v_carriage == 0)
+                    {
+                        updateText();
+                    }
+                    carriage--;
+                }
+            }
+            break;
+        case sf::Keyboard::Right:
+
+            if (!m_newText.isEmpty())
+            {
+                if (carriage != (m_newText.getSize() - 1))
+                {
+                    carriage++;
+                    if (v_carriage != 25)
+                    {
+                        v_carriage++;
+                        updateVCarriage();
+                    }
+                    else if (v_carriage == 25)
+                    {
+                        updateVCarriage();
+                        updateText();
+                    }
+                }
+            }
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+bool TextBox::Contains(const int& x, const int& y)
+{
+    return m_box.getGlobalBounds().contains(x, y);
+}
+auto TextBox::getStr()
+{
+    return m_newText.getData();
+}
+int TextBox::getSize()
+{
+    return m_newText.getSize();
+}
+
+void TextBox::setText(const sf::String& str)
+{
+    m_text.setString(str);
+}
+void TextBox::updateText()
+{
+    sf::String temp;
+    if (carriage > r_bound)
+    {
+        if (m_newText.getSize() > 25)
+        {
+            temp = m_newText.substring(carriage - 24, 25);
+            r_bound = carriage;
+            l_bound = carriage - 24;
+        }
+        else
+        {
+            r_bound = carriage;
+            l_bound = 0;
+        }
+    }
+    else if (carriage < l_bound)
+    {
+        if (m_newText.getSize() > 25)
+        {
+            temp = m_newText.substring(carriage, 25);
+            r_bound = carriage + 24;
+            l_bound = carriage;
+        }
+        else
+        {
+            r_bound = carriage;
+            l_bound = 0;
+        }
+    }
+    else
+    {
+        if (!m_newText.isEmpty())
+        {
+            temp = m_newText.substring(l_bound, 25);
+        }
+    }
+    setText(temp);
+}
+void TextBox::updateVCarriage()
+{
+    carriage_box.setPosition((m_text.findCharacterPos(v_carriage)).x + 1, 505);
+}
+
+FocusController::FocusController(FocusObject* obj) : m_object(obj)
+{
+    if (m_object != 0)
+    {
+        m_object->setFocus();
+    }
+}
+void FocusController::setFocusObject(FocusObject* new_obj)
+{
+    if (m_object == new_obj)
+        return;
+    if (m_object != 0)
+        m_object->deleteFocus();
+    if (new_obj != 0)
+        new_obj->setFocus();
+    m_object = new_obj;
+}
+FocusObject* FocusController::getFocusObject()
+{
+    return m_object;
+}
